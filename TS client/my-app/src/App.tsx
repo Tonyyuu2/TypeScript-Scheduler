1a164a13d1112e16088d4a6304c5f25ba07a5d06
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-12 16:20:07 -0700

    base layout styling done

--- TS client/my-app/src/App.tsx
+++ TS client/my-app/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
+          />
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu"></nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />
+        </section>
+        <section className="schedule"></section>
+      </main>
+    </>
   );
 }
 
--- TS client/my-app/src/stories/index.tsx
+++ TS client/my-app/src/stories/index.tsx
@@ -5,20 +5,20 @@ import { action } from "@storybook/addon-actions";
 
 import "index.scss";
 
-import Button from "components/Button";
+// import Button from "components/Button";
 
-storiesOf("Button", module)
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  })
-  .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
-  .add("Clickable", () => (
-    <Button onClick={action("button-clicked")}>Clickable</Button>
-  ))
-  .add("Disabled", () => (
-    <Button disabled onClick={action("button-clicked")}>
-      Disabled
-    </Button>
-  ));
+// storiesOf("Button", module)
+//   .addParameters({
+//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+//   })
+//   .add("Base", () => <Button>Base</Button>)
+//   .add("Confirm", () => <Button confirm>Confirm</Button>)
+//   .add("Danger", () => <Button danger>Cancel</Button>)
+//   .add("Clickable", () => (
+//     <Button onClick={action("button-clicked")}>Clickable</Button>
+//   ))
+//   .add("Disabled", () => (
+//     <Button disabled onClick={action("button-clicked")}>
+//       Disabled
+//     </Button>
+//   ));
