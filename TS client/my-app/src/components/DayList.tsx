f80a3ba147a10123f1b5e4dfff56231875ac9fd5
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-07-12 19:24:33 -0700

    add Daylist

---
+++ TS client/my-app/src/components/DayList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DayList = () => {
+  return (
+    <ul></ul>
+  )
+}
+
+export default DayList
--- TS client/my-app/src/components/DayListItem.tsx
+++ TS client/my-app/src/components/DayListItem.tsx
@@ -1,12 +1,22 @@
+import classNames from "classnames";
 import React from "react";
+import './DayListItem.css'
 
 interface Props {
   setDay: (day: string) => void;
   day: string;
   spots: number;
+  selected: boolean
 }
 
-const DayListItem = ({ setDay, day, spots }: Props) => {
+const DayListItem = ({ setDay, day, spots, selected }: Props) => {
+
+  const dayClass = classNames('day-list__item', {
+    "day-list--selected": selected,
+    "day-list--full": spots === 0
+  })
+
+
   return (
     <li onClick={() => setDay(day)}>
       <h2 className="text--regular">{day}</h2>
